451fc9c64665ffe26598275806627782e1be22ad
maryna-js <rastargueva.m@gmail.com>
2020-02-11 01:34:54 +0300

    feat: add current question functionality

--- songbird/src/components/questionBlock/questionBlock.jsx
+++ songbird/src/components/questionBlock/questionBlock.jsx
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import bird from './images/bird-guess.jpg';
 import AudioPlayer from "react-h5-audio-player";
+import questions from '../../data/quiz.json';
+import isEmpty from '../../utils/isEmpty.js';
 
 // const Player = () => (
 //   <AudioPlayer
@@ -15,9 +17,22 @@ class QuestionBlock extends Component {
     
     constructor(props) {
         super(props);
-        // this.state = {
-        //     counter: 0
-        // };
+        this.state = {
+            questions,
+            currentQuestion: {},
+            nextQuestion: {},
+            previousQuestion: {},
+            answer: '',
+            numberOfQuestions: 0,
+            numberOfAnsweredQuetions: 0,
+            currentQuestionIndex: 0,
+            score: 0,
+            correctAnswers: 0,
+            wrongAnswers: 0,
+            hints: 5,
+            fiftyFifty: 2,
+            usedFiftyFifty: false
+        };
     }
 
     // increaseCount = () => {
@@ -26,10 +41,35 @@ class QuestionBlock extends Component {
     //     });
     // }
 
+    componentDidMount () {
+        const { questions, currentQuestion, nextQuestion, previousQuestion } = this.state;
+        this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion);
+    }
+
+    displayQuestions = (questions = this.state.questions, currentQuestion, nextQuestion, previousQuestion) => {
+        let { currentQuestionIndex } = this.state;
+        if (!isEmpty(this.state.questions)) {
+            questions = this.state.questions;
+            currentQuestion = questions[currentQuestionIndex];
+            nextQuestion = questions[currentQuestionIndex + 1];
+            previousQuestion = questions[currentQuestionIndex - 1];
+            const answer = currentQuestionIndex.answer;
+            this.setState({
+                currentQuestion,
+            nextQuestion,
+            previousQuestion,
+            answer,
+
+            });
+        }
+    };
+
   render () {
+      const { currentQuestion } = this.state;
+      console.log(this.state);
       return (
         <>
-        <div>
+        <div className = "question__container">
         <div className = "question jumbotron">
         <img src={bird} alt="Quess the bird" className = "question__img" />
         {/* <p>Counter: {this.state.counter}</p>
@@ -38,14 +78,45 @@ class QuestionBlock extends Component {
                 
             <p>test</p>
             </div> */}
+            
+            {/* <p>{currentQuestion.audio}</p> */}
             <AudioPlayer
     autoPlay = {false}
-    showVolumeControl={false}
-    src="https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3"
+    
+    src={currentQuestion.audio}
     onPlay={e => console.log("onPlay")}
     // other props here
   />
            
+        </div>
+        <div className = "main__container">
+        <div className = "options__container card text-white bg-info mb-3">
+            <div class="card-header">
+              <p>Выберите ответ</p>
+              <div className = "card__lifeline">
+                  <p>
+                      <span>2 of 15</span>
+                  </p>
+              </div>
+            </div>
+            <p className = "option">Кукушка</p>
+            <p className = "option">Кукушка</p>
+            <p className = "option">Кукушка</p>
+            <p className = "option">Кукушка</p>
+            <p className = "option">Кукушка</p>
+            <p className = "option">Кукушка</p>
+
+
+        </div>
+        <div className = "description__container card border-info mb-3">
+        <div class="card-header">Послушайте плеер.</div>
+            Выберите птицу из списка
+        </div>
+        </div>
+        <div className = "button__container">
+            <button className = "btn btn-secondary">Previous</button>
+            <button className = "btn btn-success">Next</button>
+            <button className = "btn btn-danger">Quit</button>
         </div>
         </div>
         </>
